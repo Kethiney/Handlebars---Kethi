88bd60a7d539fe3f26298a2cc31450fc7c4c6b44
Kethiney <kethiney.brito@estudante.ifms.edu.br>
2024-05-23 13:38:30 -0400

    commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require ("express")
+const exphbs = require ("express-handlebars")
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+var usuarios = [
+    {
+        nome: "Talita",
+        endereco: "Av.Eurico Soares, nº 1000.",
+    },
+    {
+        nome: "Clebinho",
+        endereco: "Rua 3, Morada do Sol, nº 500",
+    },
+    {
+        nome: "Rogérinho",
+        endereco: "Rua da Saudade, nº 3000", 
+    },
+];
+
+app.get("/", (req,res) =>{
+    res.render("home", {usuarios});
+});
+
+app.get("/novo", (req,res) =>{
+    res.render("formCadastro");
+});
+
+app.get("/usuario/:id", (req,res) =>{
+    let usuario = {
+        nome: "Talita",
+        endereco: "Av.Eurico Soares, nº 1000.",
+    }
+
+    res.render("usuario", usuario);
+});
+
+app.listen(8000, () => {
+    console.log("Servidor rodando!");
+});
